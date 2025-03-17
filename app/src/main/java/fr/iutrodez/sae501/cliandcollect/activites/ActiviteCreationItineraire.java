6555bfbb57e55d2f076cbf64a29fad9e6fd336c6
Loïc Faugières <loic.faugieres@gmail.com>
2025-03-17 11:11:12 +0100

    fix(requête): nom itinéraire

--- app/src/main/java/fr/iutrodez/sae501/cliandcollect/activites/ActiviteCreationItineraire.java
+++ app/src/main/java/fr/iutrodez/sae501/cliandcollect/activites/ActiviteCreationItineraire.java
@@ -168,7 +168,7 @@ public class ActiviteCreationItineraire extends AppCompatActivity {
                     point.put("y", c.getY());
                     listePoint.put(String.valueOf(c.getID()), point);
                 }
-                jsonFinal.put("nom", inputNomItineraire.getText().toString());
+                jsonFinal.put("nomItineraire", inputNomItineraire.getText().toString());
                 jsonFinal.put("listePoint", listePoint);
             } catch (Exception e) {
                 Log.e("Itineraire", "Erreur lors de la génération du JSON : " + e);
