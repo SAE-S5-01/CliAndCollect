5d90e5454c1264f0f8cfeb0ebf4208ed3729cc8b
Loïc Faugières <loic.faugieres@gmail.com>
2025-03-19 11:14:47 +0100

    feat(parcours): ajout positions gps précédentes

--- app/src/main/java/fr/iutrodez/sae501/cliandcollect/parcoursUtils/Parcours.java
+++ app/src/main/java/fr/iutrodez/sae501/cliandcollect/parcoursUtils/Parcours.java
@@ -5,15 +5,18 @@
 
 package fr.iutrodez.sae501.cliandcollect.parcoursUtils;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 
 import fr.iutrodez.sae501.cliandcollect.clientUtils.Client;
 import fr.iutrodez.sae501.cliandcollect.clientUtils.SingletonListeClient;
 import fr.iutrodez.sae501.cliandcollect.itineraireUtils.Itineraire;
+import fr.iutrodez.sae501.cliandcollect.itineraireUtils.PointGPS;
 import fr.iutrodez.sae501.cliandcollect.itineraireUtils.SingletonListeItineraire;
 
 /**
@@ -35,6 +38,8 @@ public class Parcours {
 
     private Client dernierContactVisite;
 
+    private ArrayList<PointGPS> positionsGpsPrecedentes;
+
     public Parcours(JSONObject objetParcours) throws JSONException {
         this.id = objetParcours.getLong("id");
         this.itineraire = SingletonListeItineraire.getInstance().getItineraire(objetParcours.getString("idItineraire"));
@@ -44,11 +49,28 @@ public class Parcours {
         this.dateCreation = Date.from(offsetDateTime.toInstant());
         this.statut = objetParcours.getString("statut");
         this.nombreEtapes = itineraire != null ? itineraire.getListeCoordonnees().size() : -1;
+
         if (objetParcours.has("idDernierContactVisite")
             && !objetParcours.isNull("idDernierContactVisite")) {
             this.dernierContactVisite
             = SingletonListeClient.getInstance().getClient(
                 objetParcours.getLong("idDernierContactVisite"));
+        }
+
+        if (objetParcours.has("positionsGpsPrecedentes")
+            && !objetParcours.isNull("positionsGpsPrecedentes")) {
+            this.positionsGpsPrecedentes = new ArrayList<>();
+
+            JSONArray positionsGpsPrecedentes = objetParcours.getJSONArray("positionsGpsPrecedentes");
+
+            for (int i = 0; i < positionsGpsPrecedentes.length(); i++) {
+                JSONObject positionPrecedente = positionsGpsPrecedentes.getJSONObject(i);
+                this.positionsGpsPrecedentes.add(new PointGPS(
+                    positionPrecedente.getDouble("y"),
+                    positionPrecedente.getDouble("x"),
+                    ""
+                ));
+            }
         }
     }
 
@@ -98,5 +120,13 @@ public class Parcours {
 
     public void setDernierContactVisite(Client dernierContactVisite) {
         this.dernierContactVisite = dernierContactVisite;
+    }
+
+    public ArrayList<PointGPS> getPositionsGpsPrecedentes() {
+        return positionsGpsPrecedentes;
+    }
+
+    public void setPositionsGpsPrecedentes(ArrayList<PointGPS> positionsGpsPrecedentes) {
+        this.positionsGpsPrecedentes = positionsGpsPrecedentes;
     }
 }
